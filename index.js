9210e2109cc4efb067ee25246f9883b43ffae60d
simzeee <jwsims816@gmail.com>
2021-09-16 14:38:17 -0500

    mark's fixes

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-function carousel(){
-  let itemClassName = ""
+let apps = [
+  'app',
+  'blog',
+  'shop',
+  'note',
+  'game',
+  'grave'
+];
+
+let appName = document.getElementById('app');
+appName.innerHTML = apps[0]
+
+const toggleDark = () => {
+  document.body.style.backgroundColor = '#000000'
+  document.body.style.color = '#FFFFFF'
+  appName.style.color = 'red'
 }
+
+const toggleLight = () => {
+  document.body.style.color = '#000000'
+  document.body.style.backgroundColor = '#FFFFFF'
+  appName.style.color = 'black'
+}
+
+const rotateRight = () => {
+  apps.push(apps.shift())
+  appName.innerHTML = apps[0]
+  apps[0] === 'grave' ? toggleDark() : toggleLight();
+  return apps
+}
+
+const rotateLeft = () => {
+  apps.unshift(apps.pop())
+  appName.innerHTML = apps[0]
+  apps[0] === 'grave' ? toggleDark() : toggleLight();
+  return apps
+}
+
+document.getElementById('right-button').onclick = rotateRight
+document.getElementById('left-button').onclick = rotateLeft
